781d78bef7eaf585a2262469b448fd69986db887
Lennart <lennartvandermolen@gmail.com>
2025-03-08 20:10:44 +0100

    Create index.js

---
+++ src/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PreciseCollisionGame from './PreciseCollisionGame';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <PreciseCollisionGame />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
